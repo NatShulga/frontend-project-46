022e2240633342c400f9f7216b466df9515dce98
NatShulga <mrs.flowerfields@gmail.com>
2024-05-27 15:28:38 +0300

    add new file and modified files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/* eslint no-underscore-dangle: 0 */
+const dataFromFile = (file) => {
+  const extname = path.extname(file).slice(1);
+  const read = readFile(file);
+  return parse(read, extname);
+};
+
+const genDiff = (file1, file2, format = 'stylish') => {
+  const data1 = dataFromFile(file1);
+  const data2 = dataFromFile(file2);
+  const diffTree = getDiffTree(data1, data2);
+  return getFormat(diffTree, format);
+};
+
+export default genDiff;
