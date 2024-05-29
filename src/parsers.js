1df5b7463abac20be90e1e99a4ec66986feb71e9
NatShulga <mrs.flowerfields@gmail.com>
2024-05-29 15:10:24 +0300

    add new files

---
+++ src/parsers.js
@@ -0,0 +1,13 @@
+const parse = (content, extname) => {
+    switch (extname) {
+      case 'json':
+        return JSON.parse(content);
+      case 'yml':
+      case 'yaml':
+        return yaml.load(content);
+      default:
+        throw new Error(`Error: "${extname}" - this extname is not supported`);
+    }
+  };
+  
+  export default parse;
