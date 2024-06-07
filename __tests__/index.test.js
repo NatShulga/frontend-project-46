b2049a321404d2a83fec466a42f856db4da4df26
NatShulga <mrs.flowerfields@gmail.com>
2024-06-07 16:22:56 +0300

    modified file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,27 +1,27 @@
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
-import readFile from '../src/utils.js';
+import readFile from "../src/utils.js";
 import gendiff from "../src/index.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) =>
-  path.resolve(__dirname, "..", "__fixtures__", filename);
-
-//const readFile = (filename) => {
-  //const filePath = getFixturePath(filename);
-  //if (fs.existsSync(filePath)) {
-    //return fs.readFileSync(filePath, "utf-8");
-  //} else {
-    //throw new Error(`File "${filename}" not found.`);
-  //}
-//};
+const getFixturePath = (filename) => path.resolve(__dirname, "..", "__fixtures__", filename);
+//const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const stylish = readFile("stylishFormatTest.txt");
 const plain = readFile("plainData.txt");
 const json = readFile("jsonData.txt");
+
+//const readFile = (filename) => {
+//const filePath = getFixturePath(filename);
+//if (fs.existsSync(filePath)) {
+//return fs.readFileSync(filePath, "utf-8");
+//} else {
+//throw new Error(`File "${filename}" not found.`);
+//}
+//};
 
 describe("testing all formates", () => {
   it("default format", () => {
@@ -43,10 +43,9 @@ describe("testing all formates", () => {
   });
   it("stylish format, yaml files, json files", () => {
     expect(
-      gendiff(
-        getFixturePath("./__fixtures__/file1.yml"),
-        getFixturePath("./__fixtures__/file2.yml")
-      ).toEqual(stylish)
+      gendiff(getFixturePath("file1.yml"), getFixturePath("file2.yml")).toEqual(
+        stylish
+      )
     );
   });
   it("plain format, json files", () => {
@@ -61,8 +60,8 @@ describe("testing all formates", () => {
   it("plain format, yaml files", () => {
     expect(
       gendiff(
-        getFixturePath("file1.yaml"), 
-        getFixturePath("file2.yaml"), 
+        getFixturePath("file1.yaml"),
+        getFixturePath("file2.yaml"),
         "plain"
       )
     ).toEqual(plain);
