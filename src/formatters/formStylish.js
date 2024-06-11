f54f36886b2f320b88bce63558d4f0b59c411442
NatShulga <mrs.flowerfields@gmail.com>
2024-06-11 08:03:07 +0300

    modified

--- src/formatters/formStylish.js
+++ src/formatters/formStylish.js
@@ -17,7 +17,7 @@ const stringify = (data, depth = 1) => {
 
 const getFormatStylish = (diffTree) => {
   const iter = (tree, depth = 1) =>
-    tree.map((node) => {const createStrWithSing = (value, sign) => `${indent(depth)}${sign} ${node.key}: ${stringify(value, depth)}\n`;
+    tree.map((node) => { const createStrWithSing = (value, sign) => `${indent(depth)}${sign} ${node.key}: ${stringify(value, depth)}\n`;
       switch (node.type) {
         case 'added':
           return createStrWithSing(node.value, '+');
