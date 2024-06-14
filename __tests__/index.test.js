4c4670cc7017a16f7b8e595d672bb5264a873afa
NatShulga <mrs.flowerfields@gmail.com>
2024-06-14 10:44:02 +0300

    modified

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,35 +1,33 @@
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-import fs from "fs";
-import readFile from "../src/utils.js";
-import gendiff from "../src/index.js";
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import readFile from '../src/utils.js';
+import gendiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.resolve(__dirname, "..", "__fixtures__", filename);
+const getFixturePath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
 
-const stylish = fs.readFileSync(getFixturePath("stylishFormatTest.txt"),"utf-8");
-const plain = fs.readFileSync(getFixturePath("plainData.txt"), "utf-8");
-const json = fs.readFileSync(getFixturePath("jsonData.txt"), "utf-8");
+const stylish = fs.readFileSync(getFixturePath('stylishFormatTest.txt'),'utf-8');
+const plain = fs.readFileSync(getFixturePath('plainData.txt'), 'utf-8');
+const json = fs.readFileSync(getFixturePath('jsonData.txt'), 'utf-8');
 
-beforeAll(() => {
-  return stylish, plain, json;
-});
+beforeAll(() => stylish, plain, json);
 
-describe("check output", () => {
+describe('check output', () => {
   const testData = [
-    ["file1.json", "file2.json", "stylishFormatTest.txt"],
-    ["file1.yaml", "file2.yaml", "stylishFormatTest.txt", "stylish"],
-    ["file1.json", "file2.yaml", "plainData.txt", "plain"],
-    ["file1.yaml", "file2.json", "jsonData.txt", "json"],
+    ['file1.json', 'file2.json', 'stylishFormatTest.txt'],
+    ['file1.yaml', 'file2.yaml', 'stylishFormatTest.txt', 'stylish'],
+    ['file1.json', 'file2.yaml', 'plainData.txt', 'plain'],
+    ['file1.yaml', 'file2.json', 'jsonData.txt', 'json'],
   ];
   test.each(testData)(
-    "formatters work",
-    (file1, file2, expectedFile, format = "stylish") => {
+    'formatters work',
+    (file1, file2, expectedFile, format = 'stylish') => {
       const result = gendiff(file1, file2, format);
       const expected = readFile(expectedFile);
       expect(result).toEqual(expected);
-    }
+    },
   );
 });
